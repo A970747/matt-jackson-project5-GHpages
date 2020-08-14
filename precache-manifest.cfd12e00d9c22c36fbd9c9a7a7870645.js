dd23b77ffe8f6122964df326685c121a295850cb
a970747 <Matt.J.Jackson@gmail.com>
2020-08-14 05:22:46 -0400

    Updates

---
+++ precache-manifest.cfd12e00d9c22c36fbd9c9a7a7870645.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15b4327eeb56c24d2c2a6fb6f331085c",
+    "url": "/matt-jackson-project5-GHpages/index.html"
+  },
+  {
+    "revision": "ed94f0bd2c5bc132fe7b",
+    "url": "/matt-jackson-project5-GHpages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7a02fc9105f64e369914",
+    "url": "/matt-jackson-project5-GHpages/static/js/2.d6f0c39a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/matt-jackson-project5-GHpages/static/js/2.d6f0c39a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed94f0bd2c5bc132fe7b",
+    "url": "/matt-jackson-project5-GHpages/static/js/main.8b4756b4.chunk.js"
+  },
+  {
+    "revision": "3bff81a574c105497703",
+    "url": "/matt-jackson-project5-GHpages/static/js/runtime-main.14344e4f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/matt-jackson-project5-GHpages/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/matt-jackson-project5-GHpages/precache-manifest.cfd12e00d9c22c36fbd9c9a7a7870645.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/matt-jackson-project5-GHpages/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
